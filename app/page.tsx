91ed6ef76c4f043b7ab3d40e68ced3193c32c20d
Parth <parthp15@yahoo.com>
2024-07-12 18:21:22 +0530

    toggle button added

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,18 @@ import { FaHome } from "react-icons/fa"
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden">
-      <div className="fixed top-0 left-0 right-0 h-6 bg-[#18191B] z-50"></div>
+    <main className="relative flex justify-center items-center flex-col overflow-hidden">
+      <div className="fixed top-0 left-0 right-0 h-6 bg-[#F0F0F3] dark:bg-[#18191B] z-50"></div>
       
       <div className="bg-[#161617] h-screen pt-6 pb-6 w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: "/", icon: <FaHome />}
-        ]}/>
-        <ModeToggle />
+        <div className="bg-white dark:bg-[#111113] absolute inset-6 rounded-xl">
+          <FloatingNav navItems={[
+            {name: 'Home', link: "/", icon: <FaHome />}
+          ]}/>
+          <div className="absolute right-6 top-6"><ModeToggle /></div>
+        </div>
       </div>
-      
-      <div className="fixed bottom-0 left-0 right-0 h-6 bg-[#18191B] z-50"></div>
+      <div className="fixed bottom-0 left-0 right-0 h-6 bg-[#F0F0F3] dark:bg-[#18191B] z-50"></div>
     </main>
   );
 }
