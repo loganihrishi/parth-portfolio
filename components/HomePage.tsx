fa17552382055bd1c5ae17ef9dfaec73404df22e
Parth <parthp15@yahoo.com>
2024-07-12 23:18:58 +0530

    light and dark added

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -13,8 +13,8 @@ const HomePage = () => {
       const label = labels[i];
 
       timeline.push(
-        <div key={`line-${i}`} className='absolute h-[1px] w-[6px] bg-[#222225]' style={{ top: `${topValue}px`, left: '161px' }}></div>,
-        <p key={`label-${i}`} className='absolute text-[8px] -rotate-90 text-[#3E4044]' style={{ top: `${topValue - 5}px`, left: `${i == 0 ? 147 : 145}px` }}>{label}</p>
+        <div key={`line-${i}`} className='absolute h-[1px] w-[6px] bg-[#E8E8EC] dark:bg-[#222225]' style={{ top: `${topValue}px`, left: '161px' }}></div>,
+        <p key={`label-${i}`} className='absolute text-[8px] -rotate-90 text-[#C8C9CD] dark:text-[#3E4044]' style={{ top: `${topValue - 5}px`, left: `${i == 0 ? 147 : 145}px` }}>{label}</p>
       );
     }
 
@@ -24,21 +24,21 @@ const HomePage = () => {
   return (
     <section>
       {/* lines */}
-      <div className='absolute w-full border-t border-solid border-[#222225] top-[60px]'></div>
-      <div className='absolute h-[706px] border-l border-solid border-[#222225] left-[170px]'></div>
-      <div className='absolute h-[646px] top-[60px] border-l border-dashed border-[#222225] left-[530px]'></div>
-      <div className='absolute h-[646px] top-[60px] border-l border-dashed border-[#222225] left-[890px]'></div>
-      <div className='absolute h-[706px] border-l border-solid border-[#222225] left-[1250px]'></div>
-      <div className='absolute w-full border-t border-solid border-[#222225] top-[662px]'></div>
-      <div className='absolute w-full border-t border-solid border-[#222225] top-[706px]'></div>
+      <div className='absolute w-full border-t border-solid border-[#E8E8EC] dark:border-[#222225] top-[60px]'></div>
+      <div className='absolute h-[706px] border-l border-solid border-[#E8E8EC] dark:border-[#222225] left-[170px]'></div>
+      <div className='absolute h-[646px] top-[60px] border-l border-dashed border-[#E8E8EC] dark:border-[#222225] left-[530px]'></div>
+      <div className='absolute h-[646px] top-[60px] border-l border-dashed border-[#E8E8EC] dark:border-[#222225] left-[890px]'></div>
+      <div className='absolute h-[706px] border-l border-solid border-[#E8E8EC] dark:border-[#222225] left-[1250px]'></div>
+      <div className='absolute w-full border-t border-solid border-[#E8E8EC] dark:border-[#222225] top-[662px]'></div>
+      <div className='absolute w-full border-t border-solid border-[#E8E8EC] dark:border-[#222225] top-[706px]'></div>
 
       {/* dots */}
-      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#777B84] top-[58px] left-[168px] ring-[#111113] ring-2'></div> 
-      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#777B84] top-[660px] left-[168px] ring-[#111113] ring-2'></div> 
-      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#777B84] top-[704px] left-[168px] ring-[#111113] ring-2'></div> 
-      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#777B84] top-[58px] left-[1248px] ring-[#111113] ring-2'></div> 
-      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#777B84] top-[660px] left-[1248px] ring-[#111113] ring-2'></div> 
-      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#777B84] top-[704px] left-[1248px] ring-[#111113] ring-2'></div> 
+      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[58px] left-[168px] ring-white dark:ring-[#111113] ring-2'></div> 
+      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[660px] left-[168px] ring-white dark:ring-[#111113] ring-2'></div> 
+      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[704px] left-[168px] ring-white dark:ring-[#111113] ring-2'></div> 
+      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[58px] left-[1248px] ring-white dark:ring-[#111113] ring-2'></div> 
+      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[660px] left-[1248px] ring-white dark:ring-[#111113] ring-2'></div> 
+      <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[704px] left-[1248px] ring-white dark:ring-[#111113] ring-2'></div> 
 
       {renderTimeline()}
     </section>
