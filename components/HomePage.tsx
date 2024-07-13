dde4aef927b0e5d3bc18da04a40f060141bc8e6f
Parth <parthp15@yahoo.com>
2024-07-13 11:43:12 +0530

    pic added and placed

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,57 +1,7 @@
-// import React from 'react'
-// import pic from "../assets/Container.svg"
-
-// const HomePage = () => {
-//   const renderTimeline = () => {
-//     const startTop = 30;
-//     const step = 47;
-//     const labels = [50, 100, 150, 200, 300, 400, 500, 600, 650, 700, 750, 800, 850, 900];
-
-//     const timeline = [];
-
-//     for (let i = 0; i < labels.length; i++) {
-//       const topValue = startTop + step * i;
-//       const label = labels[i];
-
-//       timeline.push(
-//         <div key={`line-${i}`} className='absolute h-[1px] w-[6px] bg-[#E8E8EC] dark:bg-[#222225]' style={{ top: `${topValue}px`, left: '161px' }}></div>,
-//         <p key={`label-${i}`} className='absolute text-[8px] -rotate-90 text-[#C8C9CD] dark:text-[#3E4044]' style={{ top: `${topValue - 5}px`, left: `${i == 0 ? 147 : 145}px` }}>{label}</p>
-//       );
-//     }
-
-//     return timeline;
-//   };
-
-//   return (
-//     <section>
-//       {/* lines */}
-//       <div className='absolute w-full border-t border-solid border-[#E8E8EC] dark:border-[#222225] top-[60px]'></div>
-//       <div className='absolute h-[706px] border-l border-solid border-[#E8E8EC] dark:border-[#222225] left-[170px]'></div>
-//       <div className='absolute h-[646px] top-[60px] border-l border-dashed border-[#E8E8EC] dark:border-[#222225] left-[530px]'></div>
-//       <div className='absolute h-[646px] top-[60px] border-l border-dashed border-[#E8E8EC] dark:border-[#222225] left-[890px]'></div>
-//       <div className='absolute h-[706px] border-l border-solid border-[#E8E8EC] dark:border-[#222225] left-[1250px]'></div>
-//       <div className='absolute w-full border-t border-solid border-[#E8E8EC] dark:border-[#222225] top-[662px]'></div>
-//       <div className='absolute w-full border-t border-solid border-[#E8E8EC] dark:border-[#222225] top-[706px]'></div>
-
-//       {/* dots */}
-//       <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[58px] left-[168px] ring-white dark:ring-[#111113] ring-2'></div> 
-//       <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[660px] left-[168px] ring-white dark:ring-[#111113] ring-2'></div> 
-//       <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[704px] left-[168px] ring-white dark:ring-[#111113] ring-2'></div> 
-//       <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[58px] left-[1248px] ring-white dark:ring-[#111113] ring-2'></div> 
-//       <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[660px] left-[1248px] ring-white dark:ring-[#111113] ring-2'></div> 
-//       <div className='absolute h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] top-[704px] left-[1248px] ring-white dark:ring-[#111113] ring-2'></div> 
-
-//       {renderTimeline()}
-
-//       <div className={`absolute h-[104px] w-[104px] bg-white top-[200px] left-[660px] rounded-full border-[#222225]`}><img src={pic} alt="" /></div>
-      
-//     </section>
-//   )
-// }
-
-// export default HomePage
-
 import React from 'react'
+import Image from 'next/image';
+import pic from '../assets/Container.svg'
+import pic2 from '../assets/pic.jpeg'
 import './home.css'
 
 const HomePage = () => {
@@ -87,19 +37,25 @@ const HomePage = () => {
           <div className='absolute z-[2] h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] bottom-[-2px] left-[-3px] ring-white dark:ring-[#111113] ring-2'></div>
         </div>
       </div>
+
       <div className='centerbox relative flex justify-between w-full h-[602px] border-b border-solid border-[#E8E8EC] dark:border-[#222225]'>
         <div className='relative left-box h-[602px] flex-1 border-r border-solid border-[#E8E8EC] dark:border-[#222225]'>
           <div className='absolute z-[2] h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] bottom-[-2px] right-[-3px] ring-white dark:ring-[#111113] ring-2'></div>
         </div>
         <div className='center-box flex flex-row grow min-w-[714px] min-[1200px]:min-w-[1080px]'>
           <div className='basis-1/3 h-[602px] border-r border-dashed border-[#E8E8EC] dark:border-[#222225]'></div>
-          <div className='basis-1/3 h-[602px] border-r border-dashed border-[#E8E8EC] dark:border-[#222225]'></div>
+          <div className='relative basis-1/3 h-[602px] border-r border-dashed border-[#E8E8EC] dark:border-[#222225]'>
+          <div className='relative flex justify-center items-center top-[140px]'>
+            <Image src={pic} alt="" height={104} width={104} className='rounded-full' />
+          </div>
+          </div>
           <div className='basis-1/3 h-[602px]'></div>
         </div>
         <div className='relative right-box h-[602px] flex-1 border-l border-solid border-[#E8E8EC] dark:border-[#222225]'>
         <div className='absolute z-[2] h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] bottom-[-2px] left-[-3px] ring-white dark:ring-[#111113] ring-2'></div>
         </div>
       </div>
+
       <div className='bottombox relative flex justify-between w-full h-[44px] border-b border-solid border-[#E8E8EC] dark:border-[#222225]'>
         <div className='relative left-box h-[44px] flex-1 border-r border-solid border-[#E8E8EC] dark:border-[#222225]'>
           <div className='absolute z-[2] h-[5px] w-[5px] rounded-full bg-[#81838D] dark:bg-[#777B84] bottom-[-2px] right-[-3px] ring-white dark:ring-[#111113] ring-2'></div>
