afae8501e76e43d39bfb6e9042158b5f46f2427d
Parth <parthp15@yahoo.com>
2024-07-24 15:59:24 +0530

    done

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -9,9 +9,9 @@ const Skills = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   return (
-    <section className="relative bg-[#161617] h-[594px] w-full flex flex-col">
+    <section className="relative bg-[#161617] h-full w-full flex flex-col">
       <div className="bg-white dark:bg-[#111113] h-full mx-6 flex items-center justify-center">
-        <div className='skills relative flex flex-row gap-8 justify-around w-[1280px] h-full py-[55px]'>
+        <div className='skills relative flex flex-row gap-8 justify-around w-[1280px] h-full py-[120px]'>
           <div className='flex items-center justify-center h-[484px] basis-1/4'>
             <div className='relative h-[444px] w-full rounded-[3px]'>
               <div className='absolute top-0 left-0 rounded-[3px] h-full w-full border border-dashed border-[#D3D3DC] dark:border-[#303135] backdrop-blur-[20px] bg-[#E8E8EC] dark:bg-[#222225]'></div>
