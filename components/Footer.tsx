2ca4ca7225440d17236bfd3c63c6f6b725f6690f
Parth <parthp15@yahoo.com>
2024-07-26 23:15:43 +0530

    img to Image

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -111,7 +111,7 @@ const Footer = () => {
                     className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
                   >
                     <a href={info.link} target="_blank" rel="noopener noreferrer">
-                      <img src={info.img} alt="icons" width={20} height={20}/>
+                    <Image src={info.img} alt="icons" width={20} height={20} />
                     </a>
                   </div>
                 ))}
